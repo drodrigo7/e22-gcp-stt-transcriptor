9216b333be6ac5fdba119f7e3894f0105aefefda
Rodrigo Sanchez <rodrigosanchezn@hotmail.com>
2024-10-05 10:29:52 +0200

    f-string syntax

--- src/main.py
+++ src/main.py
@@ -57,8 +57,8 @@ def att(
     This command makes a request to the Speech-to-Text V2 API for a dynamic batch
     long running operation.
     '''
-    src_uri = f'gs://{bucket_name}/{src_blob.strip('/')}'
-    dst_uri = f'gs://{bucket_name}/{dst_blob.strip('/')}'
+    src_uri = f'gs://{bucket_name}/{src_blob.strip("/")}'
+    dst_uri = f'gs://{bucket_name}/{dst_blob.strip("/")}'
     tracking_file = './assets/tracking/requests/{}.json'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))
     Service.dynamic_batch_transcription(project_id, src_uri, dst_uri, tracking_file, language_code)
     return
@@ -88,7 +88,7 @@ def ctl(
     Downloads a blob or folder from Google Cloud Storage bucket into a local directory.
     '''
     saving_path = './assets/downloads/'
-    src_uri = f'gs://{bucket_name}/{src_blob.strip('/')}'
+    src_uri = f'gs://{bucket_name}/{src_blob.strip("/")}'
     Service.gcs_to_local(src_uri, saving_path, folder)
     return
 
